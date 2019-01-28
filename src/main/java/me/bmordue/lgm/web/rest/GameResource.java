75f16927209ca77fc9d69ed853f49eb62854c215
Ben Mordue <benmordue@gmail.com>
2019-01-28 15:30:25 +0000

    Update GameResource.java

--- src/main/java/me/bmordue/lgm/web/rest/GameResource.java
+++ src/main/java/me/bmordue/lgm/web/rest/GameResource.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -118,6 +119,7 @@ public class GameResource {
      */
     @DeleteMapping("/games/{id}")
     @Timed
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteGame(@PathVariable Long id) {
         log.debug("REST request to delete Game : {}", id);
 
