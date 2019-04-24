168a815bcbba8bb3bf7c034d3304fb264aacafaf
Ben Mordue <benmordue@gmail.com>
2019-04-24 15:38:39 +0100

    post orders (broken)
    missing imports

--- src/main/java/me/bmordue/lgm/web/api/TurnsService.java
+++ src/main/java/me/bmordue/lgm/web/api/TurnsService.java
@@ -2,6 +2,7 @@ package me.bmordue.lgm.web.api;
 
 import me.bmordue.lgm.repository.GameTurnRepository;
 import me.bmordue.lgm.repository.TurnOutcomeRepository;
+import me.bmordue.lgm.security.SecurityUtils;
 import me.bmordue.lgm.service.mapper.TurnOutcomeMapper;
 import me.bmordue.lgm.web.api.model.TurnOrders;
 import me.bmordue.lgm.web.api.model.TurnResultsResponse;
@@ -19,9 +20,25 @@ public class TurnsService {
 
     @Autowired
     TurnOutcomeMapper turnOutcomeMapper;
+    
+    @Autowired
+    PlayerRepository playerRepository;
+    
+    @Autowired
+    PlayerTurnRepository playerTurnRepository;
+    
+    @Autowired
+    PlayerTurnMapper playerTurnMapper;
+    
+    @Autowired
+    RulesProcessor rulesProcessor();
 
-    public void postOrders(Long id, TurnOrders turnOrders) {
-// TODO IMPLEMENTATION
+    public void postOrders(Long id, TurnOrders turnOrders) throws AuthenticationException {
+        String userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow(AuthenticationException::new);
+        Player player = playerRepository.findByLogin(userLogin).orElseThrow(AuthenticationException::new);
+        PlayerTurn playerTurn = playerTurnMapper.turnOrdersToPlayerTurn(player, turnOrders);
+        playerTurnRepository.save(playerTurn);
+        rulesProcessor.process(); // this should become asynchronous
     }
 
     public TurnResultsResponse getTurnResults(Long id) {
