8667a1b6df1bf01d9dec44f54259c906b5f6baa9
Ben Mordue <benmordue@gmail.com>
2019-04-25 21:58:40 +0100

    IDE completions for imports, methods etc

--- src/main/java/me/bmordue/lgm/web/api/GamesService.java
+++ src/main/java/me/bmordue/lgm/web/api/GamesService.java
@@ -1,11 +1,18 @@
 package me.bmordue.lgm.web.api;
 
 import me.bmordue.lgm.domain.Game;
+import me.bmordue.lgm.domain.Player;
 import me.bmordue.lgm.repository.GameRepository;
+import me.bmordue.lgm.repository.PlayerRepository;
+import me.bmordue.lgm.security.SecurityUtils;
 import me.bmordue.lgm.service.mapper.GameMapper;
 import me.bmordue.lgm.web.api.model.GameCreatedResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 class GamesService {
@@ -25,13 +32,15 @@ class GamesService {
     }
 
     void joinGame(Long id) throws AuthenticationException {
-        String userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow(AuthenticationException::new);
-        Player player = playerRepository.findByLogin(userLogin).orElseThrow(AuthenticationException::new);
-        
+        String userLogin = SecurityUtils.getCurrentUserLogin()
+            .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("bad auth"));
+        Player player = playerRepository.findByName(userLogin)
+            .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("bad auth"));
+
         Optional<Game> game = gameRepository.findById(id);
         if (game.isPresent()) {
-            game.addPlayer(player);
-            gameRepository.save(game);
-        } // else log or throw!
+            game.get().addPlayer(player);
+            gameRepository.save(game.get());
+        } // TODO: else log or throw!
     }
 }
--- src/main/java/me/bmordue/lgm/web/api/TurnsService.java
+++ src/main/java/me/bmordue/lgm/web/api/TurnsService.java
@@ -1,19 +1,23 @@
 package me.bmordue.lgm.web.api;
 
-import me.bmordue.lgm.repository.GameTurnRepository;
+import me.bmordue.lgm.domain.Player;
+import me.bmordue.lgm.domain.PlayerTurn;
+import me.bmordue.lgm.domain.RulesProcessor;
+import me.bmordue.lgm.repository.PlayerRepository;
+import me.bmordue.lgm.repository.PlayerTurnRepository;
 import me.bmordue.lgm.repository.TurnOutcomeRepository;
 import me.bmordue.lgm.security.SecurityUtils;
+import me.bmordue.lgm.service.mapper.PlayerTurnMapper;
 import me.bmordue.lgm.service.mapper.TurnOutcomeMapper;
 import me.bmordue.lgm.web.api.model.TurnOrders;
 import me.bmordue.lgm.web.api.model.TurnResultsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TurnsService {
-
-    @Autowired
-    GameTurnRepository gameTurnRepository;
 
     @Autowired
     TurnOutcomeRepository turnOutcomeRepository;
@@ -31,17 +35,19 @@ public class TurnsService {
     PlayerTurnMapper playerTurnMapper;
     
     @Autowired
-    RulesProcessor rulesProcessor();
+    RulesProcessor rulesProcessor;
 
-    public void postOrders(Long id, TurnOrders turnOrders) throws AuthenticationException {
-        String userLogin = SecurityUtils.getCurrentUserLogin().orElseThrow(AuthenticationException::new);
-        Player player = playerRepository.findByLogin(userLogin).orElseThrow(AuthenticationException::new);
-        PlayerTurn playerTurn = playerTurnMapper.turnOrdersToPlayerTurn(player, turnOrders);
+    void postOrders(Long id, TurnOrders turnOrders) throws AuthenticationException {
+        String userLogin = SecurityUtils.getCurrentUserLogin()
+            .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("bad auth"));
+        Player player = playerRepository.findByName(userLogin)
+            .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("bad auth"));
+        PlayerTurn playerTurn = playerTurnMapper.turnOrdersToPlayerTurn(id, player, turnOrders);
         playerTurnRepository.save(playerTurn);
         rulesProcessor.process(); // this should become asynchronous
     }
 
-    public TurnResultsResponse getTurnResults(Long id) {
+    TurnResultsResponse getTurnResults(Long id) {
         return turnOutcomeMapper.turnOutcomeToTurnResultsResponse(turnOutcomeRepository.getOne(id));
     }
 }
