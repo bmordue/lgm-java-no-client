46080fad97fb3bf4d37072f02edfa38c53053c1c
Ben Mordue <benmordue@gmail.com>
2019-04-29 14:41:25 +0200

    missing mapper annotation

--- src/main/java/me/bmordue/lgm/service/mapper/PlayerTurnMapper.java
+++ src/main/java/me/bmordue/lgm/service/mapper/PlayerTurnMapper.java
@@ -4,6 +4,9 @@ import me.bmordue.lgm.domain.Player;
 import me.bmordue.lgm.domain.PlayerTurn;
 import me.bmordue.lgm.web.api.model.TurnOrders;
 
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PlayerTurnMapper {
     PlayerTurn turnOrdersToPlayerTurn(Long id, Player player, TurnOrders turnOrders);
 }
