16334d17bf8b709c5162e81f51e178b4451004ed
Ben Mordue <benmordue@gmail.com>
2019-05-17 21:51:00 +0100

    fix test assertion

--- src/test/java/me/bmordue/lgm/web/api/TurnsApiDelegateImplTest.java
+++ src/test/java/me/bmordue/lgm/web/api/TurnsApiDelegateImplTest.java
@@ -60,7 +60,7 @@ public class TurnsApiDelegateImplTest {
 
         mockMvc.perform(MockMvcRequestBuilders
             .post("/turns/1").contentType("application/json").content(jsonContent))
-            .andExpect(status().isInternalServerError());
+            .andExpect(status().isUnauthorized());
     }
             
     @Test
