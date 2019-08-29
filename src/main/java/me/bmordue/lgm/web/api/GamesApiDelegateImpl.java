47c5ca55493ab4301c645358f414401e8c03c389
Ben Mordue <benmordue@gmail.com>
2019-08-29 13:00:03 +0200

    PreAuthorize apis

--- src/main/java/me/bmordue/lgm/web/api/GamesApiDelegateImpl.java
+++ src/main/java/me/bmordue/lgm/web/api/GamesApiDelegateImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @Service
 public class GamesApiDelegateImpl implements GamesApiDelegate {
@@ -14,11 +15,13 @@ public class GamesApiDelegateImpl implements GamesApiDelegate {
     GamesService gamesService;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<GameCreatedResponse> createGame() {
         return new ResponseEntity<>(gamesService.createGame(), HttpStatus.OK);
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
     @Transactional
     public ResponseEntity<Void> joinGame(Long id) {
         gamesService.joinGame(id);
--- src/main/java/me/bmordue/lgm/web/api/TurnsApiDelegateImpl.java
+++ src/main/java/me/bmordue/lgm/web/api/TurnsApiDelegateImpl.java
@@ -5,6 +5,7 @@ import me.bmordue.lgm.web.api.model.TurnResultsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +15,14 @@ public class TurnsApiDelegateImpl implements TurnsApiDelegate {
     TurnsService turnsService;
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<Void> postOrders(Long id, TurnOrders turnOrders) {
         turnsService.postOrders(id, turnOrders);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     @Override
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<TurnResultsResponse> turnResults(Long id) {
         return new ResponseEntity<>(turnsService.getTurnResults(id), HttpStatus.OK);
     }
